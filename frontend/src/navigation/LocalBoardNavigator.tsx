b619efcd7737b471347e46b08670134fdd3ad3ca
KimDoHun <thurr1394@gamil.com>
2025-05-21 13:19:22 +0900

    feature: 우리 동네 게시판(LocalBoard) 스크린 답글 스크린 생성 및 네비게이터 설정

--- frontend/src/navigation/LocalBoardNavigator.tsx
+++ frontend/src/navigation/LocalBoardNavigator.tsx
@@ -2,11 +2,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LocalBoardMain from '../screens/localBoard/LocalBoardMain';
 import LocalBoardDetail from '../screens/localBoard/LocalBoardDetail';
 import WriteLocalPost from '../screens/localBoard/WriteLocalPost';
+import LocalBoardReplyNavigator from './LocalBoardReplyNavigator';
+import { NavigatorScreenParams } from '@react-navigation/native';
+import { LocalBoardReplyStackParamList } from './LocalBoardReplyNavigator';
+import { TouchableOpacity, Text } from 'react-native';
 
 export type LocalBoardStackParamList = {
     LocalBoardMain :  undefined;
     LocalBoardDetail : { id: number };
     WriteLocalPost: undefined;
+    LocalBoardReplyStack: NavigatorScreenParams<LocalBoardReplyStackParamList>;
 }
 
 const Stack = createNativeStackNavigator<LocalBoardStackParamList>();
@@ -16,7 +21,8 @@ export default function LocalBoardNavigator() {
         <Stack.Navigator initialRouteName="LocalBoardMain">
             <Stack.Screen name="LocalBoardMain" component={LocalBoardMain} options={{ title: '우리 동네 게시판', headerShadowVisible: false}}/>
             <Stack.Screen name="LocalBoardDetail" component={LocalBoardDetail} options={{ title: '', headerShadowVisible: false}}/>
-            <Stack.Screen name="WriteLocalPost" component={WriteLocalPost} options={{ title: '', headerShadowVisible: false }}/>
+            <Stack.Screen name="WriteLocalPost" component={WriteLocalPost} options={{ title: '', headerShadowVisible: false}}/>
+            <Stack.Screen name="LocalBoardReplyStack" component={LocalBoardReplyNavigator} options={{ title: '', headerShown: false }}/>
         </Stack.Navigator>
     );
 }
