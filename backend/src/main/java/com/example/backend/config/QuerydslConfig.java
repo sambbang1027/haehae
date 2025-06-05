7993a1e8083cdcbd878fd23a6503bd3a0480c3ed
KimDoHun <thurr1394@gamil.com>
2025-06-05 13:01:58 +0900

    refactor : Reward Controller,Service,Repository,Dto 패키지 및 파일 위치 수정

--- backend/src/main/java/com/example/backend/config/QuerydslConfig.java
+++ backend/src/main/java/com/example/backend/config/QuerydslConfig.java
@@ -1,5 +1,7 @@
+
 package com.example.backend.config;
 
+import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -10,10 +12,11 @@ import org.springframework.context.annotation.Configuration;
 public class QuerydslConfig {
 
     @PersistenceContext
-    private EntityManager em;
+    private EntityManager entityManager;
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory(){
-        return new JPAQueryFactory(em);
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT ,entityManager);
     }
 }
+
