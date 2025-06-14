2fffe02689d9b4963463b8b4c063870c02fba29c
KangJaeHeon <gjsdms1244@gmail.com>
2025-06-14 18:19:01 +0900

    feat : 이미지 선택 기능 및 UI 등  Hooks 을 사용하여 전역적으로 사용가능하게 리펙토링

--- frontend/src/screens/localBoard/LocalBoardDetail.tsx
+++ frontend/src/screens/localBoard/LocalBoardDetail.tsx
@@ -18,10 +18,10 @@ import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import OptionModal from "../../components/OptionModal";
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import axios from "axios";
-import { fetchImageUrl } from '../../utils/FirebaseRead';
 import api from '../../api/AxiosInstance';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import { formatTOKSTDateTime } from "../../utils/TimeStampToConvert";
+import ImagePicker from "../../components/ImageFicker.tsx/ImagePicker";
 
 type LocalBoardDetailRouteProps = RouteProp<LocalBoardStackParamList, "LocalBoardDetail">;
 
--- frontend/src/screens/localBoard/LocalBoardMain.tsx
+++ frontend/src/screens/localBoard/LocalBoardMain.tsx
@@ -31,7 +31,7 @@ export default function LocalBoardMain() {
 
   const posts = [
     {
-      id: 42,
+      id: 46,
       author: '서샘이',
       date: '2025년 4월 24일',
       content: '서샘이의 광주 VS 강재현의 광주 누가 더 시골인인가요?',
