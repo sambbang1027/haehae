074aa8e3923fd669ac2867d0d046db8d6bfcfab1
Sammy <seosi97@gmail.com>
2025-06-25 09:00:00 +0900

    [Fix] Delete reading directory route of env file

--- backend/src/main/java/com/example/backend/webSocket/StompJwtTokenProvider.java
+++ backend/src/main/java/com/example/backend/webSocket/StompJwtTokenProvider.java
@@ -1,26 +1,40 @@
 package com.example.backend.webSocket;
 
+import com.example.backend.security.JwtTokenProvider;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 
+
 @Component
 public class StompJwtTokenProvider {
 
-    private final String secret;
+    @Autowired
+    private  JwtTokenProvider jwtTokenProvider;
 
-    public StompJwtTokenProvider() {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("C:/haehae/backend") // 또는 경로 확인
-                .filename(".env")
-                .load();
-
-        this.secret = dotenv.get("JWT_SECRET");
+    @Autowired
+    public StompJwtTokenProvider(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
+
+//    private final String secret;
+//
+//    public StompJwtTokenProvider() {
+//        Dotenv dotenv = Dotenv.configure()
+//                .directory("C:/haehae/backend") // 또는 경로 확인
+//                .filename(".env")
+//                .load();
+//        this.secret = dotenv.get("JWT_SECRET");
+//    }
+
+
     public String getUserId(String token) {
+        String secret = jwtTokenProvider.getSecret();
         return Jwts.parser()
                 .setSigningKey(secret.getBytes(StandardCharsets.UTF_8))
                 .parseClaimsJws(token)
