f763df3ab04243380b5ecfa6cb288245fe113f63
KangJaeHeon <gjsdms1244@gmail.com>
2025-06-28 14:18:50 +0900

    feat : React Query 라이브러리 추가

--- frontend/src/hooks/UsePagination.ts
+++ frontend/src/hooks/UsePagination.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import api from '../api/AxiosInstance';
 
     interface CursorPaginationProps<T> {
