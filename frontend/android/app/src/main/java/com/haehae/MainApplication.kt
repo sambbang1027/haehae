bcad3c05b7243f8726e80cfd907f8a404d0fbb55
Sammy <seosi97@gmail.com>
2025-07-07 20:58:32 +0900

    create auto login and reset password , find id

--- frontend/android/app/src/main/java/com/haehae/MainApplication.kt
+++ frontend/android/app/src/main/java/com/haehae/MainApplication.kt
@@ -12,17 +12,6 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
-<<<<<<< HEAD
-
-
-class MainApplication : Application(), ReactApplication {
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-      
-override fun getPackages(): List<ReactPackage> =
-    PackageList(this).packages.toMutableList().apply {
-    }
-=======
 class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
@@ -30,8 +19,6 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.toMutableList().apply {
 
             }
->>>>>>> 3e279cebba136c174a32d85ac0b9e3f0bef40468
-
         override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
