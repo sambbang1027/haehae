8f2649a474c755af8b0a658049c9314303b3edb0
KangJaeHeon <gjsdms1244@gmail.com>
2025-08-04 16:46:01 +0900

    refactor : 게시물,댓글 등 작성 시 유저패널티 체크 AOP 구현.

--- backend/src/main/java/com/example/backend/aop/UserPenaltyCheckAspect.java
+++ backend/src/main/java/com/example/backend/aop/UserPenaltyCheckAspect.java
@@ -2,18 +2,15 @@ package com.example.backend.aop;
 
 import com.example.backend.exception.PenaltyException;
 import com.example.backend.userPenalty.service.UserPenaltyService;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(1)
 public class UserPenaltyCheckAspect {
 
     private final UserPenaltyService userPenaltyService;
@@ -27,6 +24,7 @@ public class UserPenaltyCheckAspect {
 
 
     @Around("checkPenaltyPointcut()")
+    @Order(1)
     public Object beforeCheckPenalty(ProceedingJoinPoint joinPoint)  throws Throwable {
         Object[] args = joinPoint.getArgs();
         Long userId = null;
